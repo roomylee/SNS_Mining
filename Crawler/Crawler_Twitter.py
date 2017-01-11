224be2431f945c7e489d1052f0155d375fec3aec
Joohong Lee <roomylee@naver.com>
2017-01-12 00:35:08 +0900

    트위터에 관련된 크롤러들(트윗, 리트윗, 답글)을 통합, 최빈단어추출을 NLP.py에 메소드로 병합

--- Crawler/Crawler_Twitter.py
+++ Crawler/Crawler_Twitter.py
@@ -1,8 +1,10 @@
 # My ID : 553557460
 import tweepy
 import mysql.connector as msc
+import mysql.connector.errors
 import pandas as pd
 from datetime import datetime, timedelta
+import time
 
 # Twitter Developer Account Info.
 API_KEY = ***REMOVED***
@@ -25,84 +27,119 @@ qry = conn.cursor(buffered=True)
 # Tweet을 수집하여 twitter_tweet DB에 저장
 def collect_tweet(id):
     # tweepy.Cursor를 통해서 타임라인 게시글 탐색
-    for post in tweepy.Cursor(api.user_timeline, user_id=id).items():
-        # API user_timeline() attribute 설명
-        # screen_name : @닉네임
-        # created_at : 게시글 생성 시간
-        # id : 게시글 ID(URL)
-        # text : 게시글 내용
-        # favorite_count : 좋아요 개수
-        # retweet_count : 리트윗 개수
-        screen_name = post.author.screen_name
-        date = post.created_at + timedelta(hours=9) # 한국시간으로 변환(+9시간)
-        url = post.id
-        content = post.text
-        favorite = post.favorite_count
-        retweet = post.retweet_count
+    cursor = tweepy.Cursor(api.user_timeline, user_id=id).items()
 
-        # INSERT Query 생성
-        insert_qry = "INSERT INGNORE INTO twitter_tweet VALUES('%s', '%s', '%s', '%s', '%s', %d, %d)" \
-                    % (id, screen_name, date, url, content, favorite, retweet)
-
-        # INSERT Query 실행
+    while True:
         try:
+            post = cursor.next()
+
+            # API user_timeline() attribute 설명
+            # screen_name : @닉네임
+            # created_at : 게시글 생성 시간
+            # id : 게시글 ID(URL)
+            # text : 게시글 내용
+            # favorite_count : 좋아요 개수
+            # retweet_count : 리트윗 개수
+            screen_name = post.author.screen_name
+            date = post.created_at + timedelta(hours=9) # 한국시간으로 변환(+9시간)
+            url = post.id
+            content = post.text
+            favorite = post.favorite_count
+            retweet = post.retweet_count
+
+            content = content.replace("'","\\\'")
+
+            # INSERT Query 생성
+            insert_qry = "INSERT IGNORE INTO twitter_tweet VALUES('%s', '%s', '%s', '%s', '%s', %d, %d)" \
+                        % (id, screen_name, date, url, content, favorite, retweet)
+
+            # INSERT Query 실행
             qry.execute(insert_qry)
             conn.commit()
-        except:
-            conn.rollback()
+
+        except tweepy.TweepError:
+            print("Waits...")
+            time.sleep(60 * 15 + 1)
+            print("Wake up!")
+            continue
+
+        except mysql.connector.errors.ProgrammingError:
+            print("MySQL Programming ERROR")
+            print("Query: %s" % insert_qry)
+            continue
+
+        except StopIteration:
+            break
 
 # 매개변수의 query는 검색하고자 하는 유저의 @Screen_Name
 # 해당 유저의 게시글에 대해 retweet(리트윗) 또는 reply(답글)하고 있는 Tweet을 수집하여
 # twitter_retweet 또는 twitter_reply DB에 저장
 def collect_repost(query):
-    for post in tweepy.Cursor(api.search, q=query).items(1000):
-        # API user_timeline() attribute 설명
-        # screen_name : @닉네임
-        # created_at : 게시글 생성 시간
-        # id : 게시글 ID(URL)
-        # text : 게시글 내용
-        # favorite_count : 좋아요 개수
-        # retweet_count : 리트윗 개수
-        screen_name = post.author.screen_name
-        date = post.created_at + timedelta(hours=9)  # 한국시간으로 변환(+9시간)
-        url = post.id
-        content = post.text
-        favorite = post.favorite_count
-        retweet = post.retweet_count
+    # tweepy.Cursor를 통해서 타임라인 게시글 탐색
+    cursor = tweepy.Cursor(api.search, q=query).items(1000)
 
-        if hasattr(post, 'retweeted_status'):
-            # origin : 원본에 대한 정보
-            origin_id = post.author.id
-            origin_screen_name = post.retweeted_status.user.screen_name
-            origin_url = post.retweeted_status.id
-
-            if origin_screen_name != query:
-                continue
-
-            # INSERT Query 생성
-            insert_qry = "INSERT IGNORE INTO twitter_retweet VALUES('%s', '%s', '%s', '%s', '%s', %d, %d, %s, %s, %s)" \
-                         % (id, screen_name, date, url, content, favorite, retweet, origin_id, origin_screen_name,
-                            origin_url)
-        else:
-            # origin : 원본에 대한 정보
-            origin_id = post.in_reply_to_user_id
-            origin_screen_name = post.in_reply_to_screen_name
-            origin_url = post.in_reply_to_status_id
-
-            if origin_screen_name != query:
-                continue
-
-            # INSERT Query 생성
-            insert_qry = "INSERT IGNORE INTO twitter_reply VALUES('%s', '%s', '%s', '%s', '%s', %d, %d, %s, %s, %s)" \
-                         % (id, screen_name, date, url, content, favorite, retweet, origin_id, origin_screen_name, origin_url)
-
-        # INSERT Query 실행
+    while True:
         try:
+            post = cursor.next()
+
+            # API user_timeline() attribute 설명
+            # screen_name : @닉네임
+            # created_at : 게시글 생성 시간
+            # id : 게시글 ID(URL)
+            # text : 게시글 내용
+            # favorite_count : 좋아요 개수
+            # retweet_count : 리트윗 개수
+            screen_name = post.author.screen_name
+            date = post.created_at + timedelta(hours=9)  # 한국시간으로 변환(+9시간)
+            url = post.id
+            content = post.text
+            favorite = post.favorite_count
+            retweet = post.retweet_count
+
+            content = content.replace("'", "\\\'")
+
+            if hasattr(post, 'retweeted_status'):
+                # origin : 원본에 대한 정보
+                origin_id = post.author.id
+                origin_screen_name = post.retweeted_status.user.screen_name
+                origin_url = post.retweeted_status.id
+
+                if "@%s" % origin_screen_name != query:
+                    continue
+
+                # INSERT Query 생성
+                insert_qry = "INSERT IGNORE INTO twitter_retweet VALUES('%s', '%s', '%s', '%s', '%s', %d, %d, '%s', '%s', '%s')" \
+                             % (id, screen_name, date, url, content, favorite, retweet, origin_id, origin_screen_name, origin_url)
+            else:
+                # origin : 원본에 대한 정보
+                origin_id = post.in_reply_to_user_id
+                origin_screen_name = post.in_reply_to_screen_name
+                origin_url = post.in_reply_to_status_id
+
+                if "@%s" % origin_screen_name != query:
+                     continue
+
+                # INSERT Query 생성
+                insert_qry = "INSERT IGNORE INTO twitter_reply VALUES('%s', '%s', '%s', '%s', '%s', %d, %d, '%s', '%s', '%s')" \
+                             % (id, screen_name, date, url, content, favorite, retweet, origin_id, origin_screen_name, origin_url)
+
+            # INSERT Query 실행
             qry.execute(insert_qry)
             conn.commit()
-        except:
-            conn.rollback()
 
+        except tweepy.TweepError:
+            print("Waits...")
+            time.sleep(60 * 15 + 1)
+            print("Wake up!")
+            continue
+
+        except mysql.connector.errors.ProgrammingError:
+            print("MySQL Programming ERROR")
+            print("Query: %s" % insert_qry)
+            continue
+
+        except StopIteration:
+            break
 
 if __name__ == '__main__':
     # 타켓 정치인 목록 가져오기
@@ -110,11 +147,17 @@ if __name__ == '__main__':
     politician = qry.fetchall()
     user_list = pd.DataFrame(data=politician, columns=['Name', 'Screen_Name', 'Twitter_ID', 'Inclination'])
 
+    flag = False
     for index, row in user_list.iterrows():
         name = row['Name']
         screen_name = row['Screen_Name']
         id = row['Twitter_ID']
         inclination = row['Inclination']
+
+        # if name == "김진표":
+        #     flag = True
+        # if flag != True:
+        #     continue
 
         print("Crawling %s's tweet..." % name)
         collect_tweet(id)
@@ -123,6 +166,7 @@ if __name__ == '__main__':
         print("Crawling Repost(Retweet/Reply) to %s's tweet" % name)
         collect_repost("@%s" % screen_name)
         print("Complete Crawling Repost(Retweet/Reply) to %s's tweet" % name)
+
 
 qry.close()
 conn.close()
--- NLP.py
+++ NLP.py
@@ -1,8 +1,7 @@
 from konlpy.tag import *
-from konlpy.utils import pprint
 import mysql.connector as msc
 import pandas as pd
-
+import operator
 
 class NLP_Engine:
     def __init__(self):
@@ -17,4 +16,79 @@ class NLP_Engine:
         return POS
 
 
+def extract_frequent_words(mysql_query, is_repost=None):
+    # connect MySQL
+    config = {'user': ***REMOVED***,
+              'password': ***REMOVED***,
+              'database': ***REMOVED***}
+    conn = msc.connect(**config)
+    qry = conn.cursor(buffered=True)
 
+    # twitter DB에 대해 최빈단어 분석
+    qry.execute(mysql_query)
+    result_qry = qry.fetchall()
+    if is_repost == True:
+        twitter_df = pd.DataFrame(data=result_qry,
+                                  columns=['Twitter_ID', 'Screen_Name', 'Inclination', 'Date', 'URL', 'Contents',
+                                           'Favorite', 'Retweet', 'Origin_ID', 'Origin_Screen_Name', 'Origin_URL'])
+    else:
+        twitter_df = pd.DataFrame(data=result_qry,
+                                  columns=['Twitter_ID', 'Screen_Name', 'Inclination', 'Date', 'URL', 'Contents',
+                                           'Favorite', 'Retweet'])
+
+    # NLP Engines Predifine
+    Kkma = 0
+    Hannanum = 1
+    Mecab = 2
+    Twitter = 3
+
+    nlp = NLP_Engine()
+    word_freq = {}
+    for text in twitter_df['Contents']:
+        token = nlp.ExtractPOS(text, Mecab)
+        for word in token:
+            if word[1] in list(['NNG', 'NNP', 'VV', 'VA', 'MM', 'MAG']):
+                if word[0] in word_freq:
+                    word_freq[word[0]] = word_freq[word[0]] + 1
+                else:
+                    word_freq[word[0]] = 1
+
+    sorted_list = sorted(word_freq.items(), key=operator.itemgetter(1))
+    sorted_list.reverse()
+
+    for i in range(0, 20):
+        print("Rank %2d. %s" % (i+1, sorted_list[i]))
+
+    return sorted_list
+
+
+if __name__ == '__main__':
+    print("******** 보수 정치인 Top 20 words ********")
+    query = "Select * from twitter_tweet where Inclination = 1"
+    extract_frequent_words(query)
+    print()
+
+    print("***** 보수 정치인 리트윗 Top 20 words *****")
+    query = "Select * from twitter_retweet where Inclination = 1"
+    extract_frequent_words(query, is_repost=True)
+    print()
+
+    print("****** 보수 정치인 답글 Top 20 words ******")
+    query = "Select * from twitter_reply where Inclination = 1"
+    extract_frequent_words(query, is_repost=True)
+    print()
+
+    print("******** 진보 정치인 Top 20 words ********")
+    query = "Select * from twitter_tweet where Inclination = 2"
+    extract_frequent_words(query)
+    print()
+
+    print("***** 진보 정치인 리트윗 Top 20 words *****")
+    query = "Select * from twitter_retweet where Inclination = 2"
+    extract_frequent_words(query, is_repost=True)
+    print()
+
+    print("****** 진보 정치인 답글 Top 20 words ******")
+    query = "Select * from twitter_reply where Inclination = 2"
+    extract_frequent_words(query, is_repost=True)
+    print()
